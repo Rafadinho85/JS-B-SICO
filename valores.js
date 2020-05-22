c2a915bb35321dfe963b50839dc639a7a2973e48
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-21 21:36:00 -0600

    variables

---
+++ valores.js
@@ -0,0 +1,26 @@
+//****valores primitivos: son valores básicos que podremos usar para generar codigo****
+//valores numericos
+40
+
+//cadenas de texto
+
+"Diego de Granda"
+
+//valores boleanos nos serviran para validar cosas, asi tomar una decision si o no
+true
+false
+
+//valores vacios, valores reservados para un tipo de valor. Nos servira para generar un espacio
+//reservado para despues colocar un valor
+null
+undefined
+
+//valores no primitivos o valores tipos objetos
+//valor tipo array
+[1,2,3]
+
+//valor tipo objeto
+{nombre: "Diego"}
+
+
+
---
+++ variables.js
@@ -0,0 +1,11 @@
+//representacion de un lugar en memoria 
+
+var nombre = "Jery";
+
+//declarar 
+
+var edad;
+
+//inicializar variable
+
+edad = 18;
