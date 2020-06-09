27bf74378a7974cc408ddb3dcf985d6b63eb8751
Jery Ramirez <rafa85jogo@gmail.com>
2020-06-08 21:30:56 -0600

    ciclo for

---
+++ for.js
@@ -0,0 +1,18 @@
+var crushs = ["Michel", "Kathy", "Rocio", "Jacky"];
+
+function saludarCrush(crush){
+    console.log(`Hola, ${crush}`);
+}
+for(var i=0; i < crushs.length; i++){
+    saludarCrush(crushs[i]);
+}
+
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes (estudiante){
+    console.log(`Hola, ${estudiante}`);
+
+}
+for(var i=0; i < estudiantes.length; i++){
+    saludarEstudiantes(estudiantes[i]);
+}
